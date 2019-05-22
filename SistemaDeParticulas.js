94afb35cffa90586ebf854b40432d0b25e496a4f
eal-iad-programacion <49421468+eal-iad-programacion@users.noreply.github.com>
2019-05-22 18:26:16 -0500

    particles property initialized

--- SistemaDeParticulas.js
+++ SistemaDeParticulas.js
@@ -1,6 +1,10 @@
 class SistemaDeParticulas{
   constructor(cantidad_de_particulas){
     this.cantidad_de_particulas = cantidad_de_particulas;
+    this.particulas=[];
+    for(let i=0; i< cantidad_de_particulas; i++){
+      particulas.push(new Paricula());
+    }
   }
   aplicarFuerza(fuerza){}
   //cambio en la rama1
