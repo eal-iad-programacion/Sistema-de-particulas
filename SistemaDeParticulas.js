be071d642ed8c1b568eecdda99f5e9b3de307af0
eal-iad-programacion <49421468+eal-iad-programacion@users.noreply.github.com>
2019-05-22 18:15:56 -0500

    amount of particles property added to SDP

--- SistemaDeParticulas.js
+++ SistemaDeParticulas.js
@@ -1,5 +1,7 @@
 class SistemaDeParticulas{
-  constructor(){}
+  constructor(particle_amount){
+    this.particle_amount=particle_amount;
+  }
   aplicarFuerza(fuerza){}
   //cambio en la rama1
   mover(){}
