f0fd3e73e147b9b97c6c225d6797a1052b578c0b
eal-iad-programacion <eal.iad.programacion@gmail.com>
2019-05-22 17:59:23 -0500

    definitivo despues de errores

--- sketch.js
+++ sketch.js
@@ -4,9 +4,5 @@ function setup() {
 
 function draw() {
   background(220);
-<<<<<<< HEAD
-  let sdp = new SistemaDeParticulas(100);
-=======
   var sdp = new SistemaDeParticulas(50);
->>>>>>> 77da54bc9572c659cc561d9af3d0c48c497951a1
 }
