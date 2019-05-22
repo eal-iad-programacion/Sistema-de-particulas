aebb3cb56c333aaf61f39de538eafc46262aa42f
eal-iad-programacion <eal.iad.programacion@gmail.com>
2019-05-22 18:24:10 -0500

    se inicio la propiedad particulas en SDP

--- SistemaDeParticulas.js
+++ SistemaDeParticulas.js
@@ -1,6 +1,10 @@
 class SistemaDeParticulas{
   constructor(cantidad_de_particulas){
     this.cantidad_de_particulas = cantidad_de_particulas;
+    this.particulas=[];
+    for(let i=0; i<this.cantidad_de_particulas;i++){
+      this.particulas.push(new Particula());
+    }
   }
   aplicarFuerza(fuerza){}
   //cambio en la rama1
