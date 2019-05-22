49b1bdc6c0eb633f20c10b0826bbb18f15a834f5
eal-iad-programacion <eal.iad.programacion@gmail.com>
2019-05-22 18:17:44 -0500

    se agrego la propiedad cantidad de particulas

--- SistemaDeParticulas.js
+++ SistemaDeParticulas.js
@@ -1,5 +1,7 @@
 class SistemaDeParticulas{
-  constructor(){}
+  constructor(cantidad_de_particulas){
+    this.cantidad_de_particulas = cantidad_de_particulas;
+  }
   aplicarFuerza(fuerza){}
   //cambio en la rama1
   mover(){}
