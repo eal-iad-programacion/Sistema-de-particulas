87e1f5f051226557c201fe57beffe3f0f00212c9
eal-iad-programacion <eal.iad.programacion@gmail.com>
2019-05-22 20:48:17 -0500

    particula basica

--- Particula.js
+++ Particula.js
@@ -2,5 +2,16 @@ class Particula{
   constructor(){
     this.posicion  = createVector();
     this.velocidad = createVector();
+    this.aceleracion= createVector();
+    this.radio=10;
+    this.color=0;
+  }
+  move(){
+    this.velocidad.add(this.aceleracion);
+    this.posicion.add(this.velocidad);
+  }
+  mostrar(){
+    fill(this.color);
+    circle(this.posicion.x,this.posicion.y,this.radio);
   }
 }
